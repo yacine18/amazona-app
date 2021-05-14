9bd4064508dce0be9f4ba31cdd7a1cc3451d91f9
unknown <hilali.yacin@gmail.com>
2021-05-14 19:16:54 +0000

    amazona-ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import data from './data'
+
+const App = () => {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div className="row">
+          <a className="brand" href="/">amazona</a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/singin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {
+           data.products.map(product => (
+              <div className="card" key={product._id}>
+                <a href={`/product/${product._id}`}>
+                  <img className="medium" src={product.image} alt={product.name} />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star"></i></span>
+                    <span><i className="fa fa-star-0"></i></span>
+                  </div>
+                  <div>
+                    <div className="price">${product.price}</div>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+
+
+        </div>
+      </main>
+      <footer className="row center">
+        All right reserved
+    </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name:'iPhone 12 Pro',
+            category: 'Phones',
+            image: '/images/product-1.jpg',
+            price: 1200,
+            brand: 'Apple',
+            numReviews: 4.5,
+            description: 'iPhone 12 Pro Max'
+        },
+        {
+            _id: '2',
+            name:'iPhone 12 Pro',
+            category: 'Phones',
+            image: '/images/product-1.jpg',
+            price: 1200,
+            brand: 'Apple',
+            numReviews: 4.5,
+            description: 'iPhone 12 Pro Max'
+        },
+        {
+            _id: '3',
+            name:'iPhone 12 Pro',
+            category: 'Phones',
+            image: '/images/product-1.jpg',
+            price: 1200,
+            brand: 'Apple',
+            numReviews: 4.5,
+            description: 'iPhone 12 Pro Max'
+        },
+        {
+            _id: '4',
+            name:'iPhone 12 Pro',
+            category: 'Phones',
+            image: '/images/product-1.jpg',
+            price: 1200,
+            brand: 'Apple',
+            numReviews: 4.5,
+            description: 'iPhone 12 Pro Max'
+        },
+        {
+            _id: '5',
+            name:'iPhone 12 Pro',
+            category: 'Phones',
+            image: '/images/product-1.jpg',
+            price: 1200,
+            brand: 'Apple',
+            numReviews: 4.5,
+            description: 'iPhone 12 Pro Max'
+        },
+        {
+            _id: '6',
+            name:'iPhone 12 Pro',
+            category: 'Phones',
+            image: '/images/product-1.jpg',
+            price: 1200,
+            brand: 'Apple',
+            numReviews: 4.5,
+            description: 'iPhone 12 Pro Max'
+        },
+    ]
+}
+
+export default data
