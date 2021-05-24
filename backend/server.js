39d76ce2ab36d0675901a920cf748e398c824a77
unknown <hilali.yacin@gmail.com>
2021-05-24 16:13:23 +0100

    publish-to-heroku

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,8 @@ app.use('/api/orders', orderRouter)
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req,res)=> res.sendFile(path.join(__dirname, '/frontend/build/index.html')))
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
 })
